14f2791713f8e633b767493eaa8c0246480c3bd5
Joseph Lau <lau.joseph.ca@gmail.com>
2025-01-18 02:28:24 +0800

    edit

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-fetch("http://www.omdbapi.com/?i=tt3896198&apikey=ffd49fd7")
-    .then(res => res.json())
-    .then(data => {
+const searchBtn = document.getElementById("search-btn")
+const searchBar = document.getElementById("search-bar")
+
+searchBtn.addEventListener("click", fetchResults)
+
+async function fetchResults() {
+    try {
+        const res = await fetch("http://www.omdbapi.com/?i=tt3896198&apikey=ffd49fd7")
+        const data = await res.json()
         console.log(data)
-        console.log(data.actors)
-    })
+    } catch (error) {
+        console.error("error", error)
+    }
+}
