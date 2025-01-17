c9e3247c42ab08fed2e56c4010d99fc19e6ad076
Joseph Lau <lau.joseph.ca@gmail.com>
2025-01-18 03:00:04 +0800

    edit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const searchBtn = document.getElementById("search-btn")
 const searchBar = document.getElementById("search-bar")
+const movieList = document.getElementById("movie-list")
 
 searchBtn.addEventListener("click", fetchResults)
 
@@ -8,6 +9,20 @@ async function fetchResults() {
         const res = await fetch("http://www.omdbapi.com/?i=tt3896198&apikey=ffd49fd7")
         const data = await res.json()
         console.log(data)
+        console.log(data.Title, data.ImdbRating, data.Runtime, data.Genre, data.Plot)
+        if (searchBar.value.includes(data.Title)) {
+            movieList.innerHTML = `
+            <div class="movie-details">
+                <div class="img-wrapper"></div>
+                <div class="details-wrapper">
+                    <h3 class="movie-title">${data.Title}</h3>
+                    <h4 class="movie-runtime">${data.Runtime}</h4>
+                    <h4 class="movie-genre">${data.Genre}</h4>
+                    <p class="movie-plot">${data.Plot}</p>
+                </div>
+            </div>
+        `
+        }
     } catch (error) {
         console.error("error", error)
     }
